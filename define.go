f6314a04571b7c63a52af3dc8ff5e106e3eec51c
masataka.hisasue <masataka.hisasue@example.com]>
2017-04-18 19:02:06 +0900

    データベースモデルの定義

--- define.go
+++ define.go
@@ -5,12 +5,33 @@ import(
 )
 
 type Music struct{
-    ID              uint64 `json:"id" gorm:"primary_key`
+    ID              uint64      `json:"id" gorm:"primary_key`
+    MusicId         string      `json:"music_id"`
+    MusicName       string      `json:"music_name"`
+    Content         string      `json:"content"`
+    Description     string      `json:"description"`
+    FavoritedUsers  []string    `json:"favorited_users"`
+    CreatedAt       time.Time   `json:"createdAt"`
+    CreatedUser     string      `json:"created_user"`
+    Tags            []string    `json:"tags"`
+    CreatingMusics  []CreatingMusic `json:"creating_musics"`
+}
+
+type User struct{
+    ID              uint64 `json:"id" gorm:"primary_key"`
+    UserId          string `json:"user_id"`
+    AccountName     string `json:"music_name"`
+    Follow          string `json:"follow"`
+    Follower        string `json:"follower"`
+    Notification    []string `json:"notification"`
+    Favorited       []string `json:"favorited"`
+    History         []string `json:"history"`
+}
+
+type CreatingMusic struct{
+    MusicId         string `json:"music_id"`
     MusicName       string `json:"music_name"`
     Content         string `json:"content"`
     Description     string `json:"description"`
-    FavoritedUsers  string `json:"favorited_users"`
-    CreatedAt       time.Time `json:"createdAt"`
-    CreatedUser     string `json:"created_user"`
-    Tags            string `json:"tags"`
+    Tags            []string `json:"tags"`
 }
