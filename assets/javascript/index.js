2e0bbb60ce09ad04f84a19ef5391dddc1d3c4e10
masataka.hisasue <masataka.hisasue@example.com]>
2017-05-18 19:11:49 +0900

    htmlに要素表示

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,21 +1,22 @@
-function test(){
-    var xhr=new XMLHttpRequest();
-    xhr.open("POST","http://localhost:1323/aaaa123/music");
-    xhr.setRequestHeader('Content-Type', 'application/json' );
-    var data = {
-        music_id : "test2",
-        music_name : "testttaaa",
-        content : "faillllf"
-    } 
-    xhr.onload=function(){console.log(xhr.response);}
-    xhr.send(JSON.stringify(data));
+window.onload = function () {
+    var mainContents = new Vue({
+        el:'#MainContents',    
+        data:{
+            musics:[]
+        },
+        beforeCreate:function(){
+            self = this;
+            var xhr=new XMLHttpRequest();
+            xhr.open("GET","http://localhost:1323/music/new");
+            xhr.setRequestHeader( 'Content-Type', 'application/json' );
+            xhr.responseType = 'json';
+            xhr.onload=function(){
+                self.musics = xhr.response;
+            }
+            xhr.send();
+            test();
+        },
+        methods:{
+        }
+    })
 }
-
-function test2(){
-    var xhr=new XMLHttpRequest();
-    xhr.open("GET","http://localhost:1323/music/new");
-    xhr.setRequestHeader( 'Content-Type', 'application/json' );
-    xhr.onload=function(){console.log(xhr.response);}
-    xhr.send();
-}
-
---
+++ assets/javascript/sub.js
@@ -0,0 +1,27 @@
+function test(){
+    var xhr=new XMLHttpRequest();
+    xhr.open("POST","http://localhost:1323/aaaa123/music");
+    xhr.setRequestHeader('Content-Type', 'application/json' );
+    var data = {
+        music_id : "test2",
+        music_name : "testttaaa",
+        content : "faillllf"
+    } 
+    xhr.onload=function(){console.log(xhr.response);}
+    xhr.send(JSON.stringify(data));
+}
+
+function test2(){
+    var xhr=new XMLHttpRequest();
+    xhr.open("GET","http://localhost:1323/music/new");
+    xhr.setRequestHeader( 'Content-Type', 'application/json' );
+    xhr.onload=function(){console.log(xhr.response);}
+    xhr.send();
+}
+
+function buildXMLJson(type,url,data,loaded){
+    var xhr=new XMLHttpRequest();
+    xhr.open(type,url);
+    xhr.setRequestHeader( 'Content-Type', 'application/json' );
+    xhr.send();
+}
